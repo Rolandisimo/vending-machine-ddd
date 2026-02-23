fba18566276eacde05fee4358e293b3cb732ca0c
Rolands Jegorovs <rolandjegorov@gmail.com>
2026-02-23 16:29:25 +0200

    Update coin.model.ts

--- src/coin/coin.model.ts
+++ src/coin/coin.model.ts
@@ -1,3 +1,4 @@
 // algorithm implicitly dependent on the order of coins here
+// needs improving but not yet sure how
 export const validCoins = [50, 20, 10, 5, 2, 1];
 export type ValidCoin = (typeof validCoins)[number];
